2261638cb12bfb3e5435596047076f049f67c3c4
Kamil S <kamil9431@gmail.com>
2020-04-20 20:46:24 +0200

    all changes

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+	region = var.aws_region
+}
+
+resource "aws_instance" "graphana-influxdb" {
+	ami = var.ami_id
+	instance_type = var.instance_type
+	vpc_security_group_ids = [aws_security_group.instance.id]
+	user_data = data.template_file.user_data.rendered
+
+	root_block_device {
+		volume_size = var.ebs_volume_system_size
+		delete_on_termination = var.delete_on_termination
+	}
+	ebs_block_device {
+		device_name = var.ebs_device_name
+		volume_size = var.ebs_volume_data_size
+		delete_on_termination = false
+	}	
+}	
+
+
+
+
---
+++ sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "instance" {
+        name = var.security_group_instance_name
+        description = "Security group for grafana and influx instance"
+	egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+        }
+}
+
+resource "aws_security_group_rule" "grafana" {
+	type = "ingress"
+	from_port = var.grafana_port 
+	to_port = var.grafana_port
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.instance.id
+}
+resource "aws_security_group_rule" "influxdb" {
+	type = "ingress"
+	from_port = var.influx_port
+	to_port = var.influx_port
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.instance.id
+}
+
